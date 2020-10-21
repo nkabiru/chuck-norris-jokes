bef8faa8c0cab7998d71776f3f1373791737978d
Njenga Kabiru <njengakabiru@gmail.com>
2020-10-21 10:13:03 +0300

    Apply fixes from StyleCI (#2)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -7,7 +7,7 @@ class JokeFactory
     protected $jokes = [
         'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.',
         'Chuck Norris counted to infinity… twice.',
-        'Once a cobra bit Chuck Norris’ leg. After five days of excruciating pain, the cobra died.'
+        'Once a cobra bit Chuck Norris’ leg. After five days of excruciating pain, the cobra died.',
     ];
 
     public function __construct(array $jokes = null)
