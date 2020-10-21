08bb9888be458462290a11659cfd923c80d19ac6
Njenga Kabiru <nkabiru@users.noreply.github.com>
2020-10-21 07:12:56 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -11,7 +11,7 @@ class JokeFactoryTest extends TestCase
     public function it_should_return_true()
     {
         $jokes = new JokeFactory([
-            'This is a joke'
+            'This is a joke',
         ]);
 
         $joke = $jokes->getRandomJoke();
@@ -25,7 +25,7 @@ class JokeFactoryTest extends TestCase
         $chuckNorrisJokes = [
             'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.',
             'Chuck Norris counted to infinity… twice.',
-            'Once a cobra bit Chuck Norris’ leg. After five days of excruciating pain, the cobra died.'
+            'Once a cobra bit Chuck Norris’ leg. After five days of excruciating pain, the cobra died.',
         ];
 
         $jokes = new JokeFactory();
